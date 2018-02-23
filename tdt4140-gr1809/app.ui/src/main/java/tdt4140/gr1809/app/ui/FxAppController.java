966b5803a3f2b81cc2ce18dc5c4852dc4dcd21c2
filiphag <filiphag@stud.ntnu.no>
2018-02-23 14:09:20 +0100

    Login UI og imports for fxml

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/FxAppController.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/FxAppController.java
@@ -1,4 +1,25 @@
 package tdt4140.gr1809.app.ui;
 
-public class FxAppController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class FxAppController implements Initializable {
+	@FXML private TextField testTextField;
+	
+	
+	
+	@FXML
+	private void initialLoginRequest() {
+		testTextField.setText(testTextField.getText() + "hei");
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// Kode som starter når man åpner applikasjonen
+		
+	}
 }
