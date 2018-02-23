5d638790e7bf063883ff214248b9dbd1c783f04f
filiphag <filiphag@stud.ntnu.no>
2018-02-23 14:26:30 +0100

    create user button and login button

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/FxAppController.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/FxAppController.java
@@ -8,13 +8,24 @@ import java.util.ResourceBundle;
 
 
 public class FxAppController implements Initializable {
-	@FXML private TextField testTextField;
-	
+	@FXML private TextField UsernameTextfield;
+	@FXML private TextField PasswordTextfield;
 	
 	
 	@FXML
 	private void initialLoginRequest() {
-		testTextField.setText(testTextField.getText() + "hei");
+		String username = UsernameTextfield.getText();
+		String password = PasswordTextfield.getText();
+		
+		//Login request to controller
+	}
+	
+	@FXML
+	private void initialCreateuserRequest() {
+		String username = UsernameTextfield.getText();
+		String password = PasswordTextfield.getText();
+		
+		//Create User request to controller in main java code
 	}
 
 	@Override
