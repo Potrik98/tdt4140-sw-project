f0a1a3ef5fd5ae292f1e681550b9578badbcd648
filiphag <filiphag@stud.ntnu.no>
2018-02-27 15:25:23 +0100

    added input-sjekk på login siden

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/FxAppController.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/FxAppController.java
@@ -10,27 +10,77 @@ import java.util.ResourceBundle;
 public class FxAppController implements Initializable {
 	@FXML private TextField UsernameTextfield;
 	@FXML private TextField PasswordTextfield;
+	@FXML private Label LoginStatus;
 	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// If you want something to run when the application starts, put it here
+		
+	}
 	
 	@FXML
 	private void initialLoginRequest() {
-		String username = UsernameTextfield.getText();
-		String password = PasswordTextfield.getText();
-		
-		//Login request to controller
+		LoginStatus.setText("");
+		String username = getLoginUsername();
+		String password = getLoginPassword();
+		if(username != null && password != null) {
+			//TODO Login request to controller
+		}
 	}
 	
 	@FXML
 	private void initialCreateuserRequest() {
-		String username = UsernameTextfield.getText();
-		String password = PasswordTextfield.getText();
-		
-		//Create User request to controller in main java code
+		LoginStatus.setText("");
+		String username = getLoginUsername();
+		String password = getLoginPassword();
+		if(username != null && password != null) {
+			//TODO Create User request to controller in main java code
+		}
 	}
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// Kode som starter når man åpner applikasjonen
+	private void WrongLoginCredentials() {
+		//Clear login textboxes (use if username/password is wrong)
+		UsernameTextfield.setText("");
+		PasswordTextfield.setText("");
+		LoginStatus.setText("Wrong login credentials");
+	}
+	
+	private void CorrectLoginCredentials() {
+		LoginStatus.setText("Welcome " + UsernameTextfield.getText());
+		UsernameTextfield.setText("");
+		PasswordTextfield.setText("");
 		
+		//TODO kode for å åpne vinduet som gir info til brukeren
+	}
+	
+	private String getLoginUsername() {
+		String username = UsernameTextfield.getText();
+		if(!username.matches("[A-Za-z0-9]+")) {
+			LoginStatus.setText("Username contains invalid characters");
+			return null;
+		}
+		else if(username.length() < 15 && username.length() < 1) {
+			LoginStatus.setText("Username is of incorrect length");
+			return null;
+		}
+		else {
+			return username;
+		}
+	}
+	
+	private String getLoginPassword() {
+		String password = PasswordTextfield.getText();
+		if(!password.matches("[A-Za-z0-9]+")) {
+			LoginStatus.setText("Password contains invalid characters");
+			return null;
+		}
+		else if(password.length() < 15 && password.length() < 1) {
+			LoginStatus.setText("Password is of incorrect length");
+			return null;
+		}
+		else {
+			return password;
+		}
 	}
 }
