c8a9025fdfe5e85612940a00aab4125316a5477b
filiphag <filiphag@stud.ntnu.no>
2018-03-02 12:22:05 +0100

    la til noen linjer i test

--- tdt4140-gr1809/app.ui/src/test/java/tdt4140/gr1809/app/ui/FxAppTest.java
+++ tdt4140-gr1809/app.ui/src/test/java/tdt4140/gr1809/app/ui/FxAppTest.java
@@ -1,15 +1,22 @@
 package tdt4140.gr1809.app.ui;
 
+import static org.junit.Assert.assertThat;
+
 import org.junit.BeforeClass;
+
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class FxAppTest extends ApplicationTest {
+	@FXML private TextField UsernameTextfield;
+	@FXML private TextField PasswordTextfield;
 	
 	@BeforeClass
 	public static void headless() {
@@ -36,5 +43,6 @@ public class FxAppTest extends ApplicationTest {
 
     @Test
     public void testFxApp() {
+    	
     }
 }
