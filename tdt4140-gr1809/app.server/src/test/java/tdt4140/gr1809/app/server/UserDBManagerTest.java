f809f99f760d708e581b7d70fdc7dbff16837c12
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-02 11:17:33 +0100

    Add UserDBManagerTest

---
+++ tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/UserDBManagerTest.java
@@ -0,0 +1,96 @@
+package tdt4140.gr1809.app.server;
+
+import org.junit.Before;
+import org.junit.Test;
+import tdt4140.gr1809.app.core.model.User;
+import tdt4140.gr1809.app.server.dbmanager.UserDBManager;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDBManagerTest extends DBManagerTest {
+    private UserDBManager dbManager;
+
+    public UserDBManagerTest() {
+        super();
+        dbManager = new UserDBManager();
+    }
+
+    @Test
+    public void testCreateAndGetUser() {
+        final User user = new User(
+                "Firstname",
+                "Lastname",
+                "gender",
+                LocalDateTime.now()
+        );
+
+        dbManager.putUser(user);
+
+        final Optional<User> retrievedUser = dbManager.getUserById(user.getId());
+
+        assertThat(retrievedUser).isPresent();
+        assertThat(retrievedUser.get()).isEqualTo(user);
+    }
+
+    @Test
+    public void testGetUserInvalidId() {
+        final UUID invalidUserId = UUID.randomUUID();
+
+        final Optional<User> retrievedUser = dbManager.getUserById(invalidUserId);
+
+        assertThat(retrievedUser).isEmpty();
+    }
+
+    @Test
+    public void testUpdateUser() {
+        final User user = new User(
+                "Firstname",
+                "Lastname",
+                "gender",
+                LocalDateTime.now()
+        );
+
+        dbManager.putUser(user);
+
+        final User updatedUser = User.from(user)
+                .firstName("Newfirstname")
+                .lastName("Newlastname")
+                .gender("Gender")
+                .birthDate(LocalDateTime.now())
+                .build();
+
+        dbManager.putUser(user);
+
+        final Optional<User> retrievedUser = dbManager.getUserById(user.getId());
+
+        assertThat(retrievedUser).isPresent();
+        assertThat(retrievedUser.get()).isEqualTo(updatedUser);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        final User user = new User(
+                "Firstname",
+                "Lastname",
+                "gender",
+                LocalDateTime.now()
+        );
+
+        dbManager.putUser(user);
+
+        final Optional<User> retrievedUser = dbManager.getUserById(user.getId());
+
+        assertThat(retrievedUser).isPresent();
+        assertThat(retrievedUser.get()).isEqualTo(user);
+
+        dbManager.deleteUser(user.getId());
+
+        final Optional<User> deletedUser = dbManager.getUserById(user.getId());
+
+        assertThat(deletedUser).isEmpty();
+    }
+}
