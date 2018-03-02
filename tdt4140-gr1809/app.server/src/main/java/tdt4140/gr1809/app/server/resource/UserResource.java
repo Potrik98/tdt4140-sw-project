7d15c1a0215831a1061629f6e31f2df0e0088785
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-02 13:25:38 +0100

    Add exception mapping and parameter extraction

--- tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/resource/UserResource.java
+++ tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/resource/UserResource.java
@@ -3,20 +3,25 @@ package tdt4140.gr1809.app.server.resource;
 import spark.Request;
 import spark.Response;
 
+import java.util.UUID;
+
 public class UserResource {
     public static void init() {
 
     }
 
     public static Response getUserById(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params("id"));
         return res;
     }
 
     public static Response putUser(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params("id"));
         return res;
     }
 
     public static Response deleteUser(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params("id"));
         return res;
     }
 }
