10be64fb59cef4a3fcf536c4cf86151df45a3346
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-02 12:45:32 +0100

    Add user resource skeleton and rest endpoints

--- tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/Server.java
+++ tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/Server.java
@@ -1,11 +1,18 @@
 package tdt4140.gr1809.app.server;
 
+import tdt4140.gr1809.app.server.resource.UserResource;
+
 import static spark.Spark.*;
 
 public class Server {
     public static void main(String[] args) {
         port(80);
         System.out.println("Starting server...");
-        get("/hello", (req, res) -> "Hello");
+        path("/user", () -> {
+            get("/:id", UserResource::getUserById);
+            put("/:id", UserResource::putUser);
+            post("/:id", UserResource::putUser);
+            delete("/:id", UserResource::deleteUser);
+        });
     }
 }
---
+++ tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/resource/UserResource.java
@@ -0,0 +1,22 @@
+package tdt4140.gr1809.app.server.resource;
+
+import spark.Request;
+import spark.Response;
+
+public class UserResource {
+    public static void init() {
+
+    }
+
+    public static Response getUserById(Request req, Response res) {
+        return res;
+    }
+
+    public static Response putUser(Request req, Response res) {
+        return res;
+    }
+
+    public static Response deleteUser(Request req, Response res) {
+        return res;
+    }
+}
