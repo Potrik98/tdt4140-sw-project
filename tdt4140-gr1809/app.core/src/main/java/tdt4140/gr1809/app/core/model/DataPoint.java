cc65cf2226a13394b908550716344f634fc2ee77
formalatist <formalatist@gmail.com>
2018-03-02 13:01:38 +0100

    ade values and time final

--- tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/model/DataPoint.java
+++ tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/model/DataPoint.java
@@ -3,8 +3,8 @@ package tdt4140.gr1809.app.core.model;
 import java.time.LocalDateTime;
 
 public class DataPoint {
-	public LocalDateTime time;
-	public int[] values;
+	public final LocalDateTime time;
+	public final int[] values;
 
 	public DataPoint(LocalDateTime time, int[] values) {
 		this.time = time;
