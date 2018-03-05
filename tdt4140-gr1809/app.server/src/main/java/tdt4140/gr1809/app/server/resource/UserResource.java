8604bd08ebafc9e3d0f2c860d730f1d82bfbaa3e
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-05 10:23:38 +0100

    Implement getUserById endpoint in resource

--- tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/resource/UserResource.java
+++ tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/resource/UserResource.java
@@ -1,17 +1,36 @@
 package tdt4140.gr1809.app.server.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.jetty.http.HttpStatus;
 import spark.Request;
 import spark.Response;
+import tdt4140.gr1809.app.core.model.User;
+import tdt4140.gr1809.app.server.dbmanager.UserDBManager;
 
+import java.sql.SQLException;
+import java.util.Optional;
 import java.util.UUID;
 
 public class UserResource {
-    public static void init() {
+    private static UserDBManager dbManager;
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .findAndRegisterModules();
 
+    public static void init() throws SQLException {
+        dbManager = new UserDBManager();
     }
 
-    public static Response getUserById(Request req, Response res) {
+    public static Response getUserById(Request req, Response res) throws Exception {
         UUID userId = UUID.fromString(req.params("id"));
+        Optional<User> user = dbManager.getUserById(userId);
+
+        if (user.isPresent()) {
+            res.status(HttpStatus.OK_200);
+            res.body(mapper.writeValueAsString(user));
+        } else {
+            res.status(HttpStatus.NOT_FOUND_404);
+        }
+
         return res;
     }
 
