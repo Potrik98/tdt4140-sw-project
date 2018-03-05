d09e6c7e04c936fefe53d9b53c2e3a7f785f5f38
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-05 13:21:25 +0100

    Add db test

--- tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/DBManagerTest.java
+++
@@ -1,9 +0,0 @@
-package tdt4140.gr1809.app.server;
-
-import tdt4140.gr1809.app.server.dbmanager.DBManager;
-
-public abstract class DBManagerTest {
-    protected DBManagerTest() {
-
-    }
-}
--- tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/UserDBManagerTest.java
+++ tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/UserDBManagerTest.java
@@ -2,27 +2,51 @@ package tdt4140.gr1809.app.server;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import tdt4140.gr1809.app.core.model.User;
 import tdt4140.gr1809.app.server.dbmanager.UserDBManager;
 
-public class UserDBManagerTest extends DBManagerTest {
+public class UserDBManagerTest {
     private UserDBManager dbManager;
 
-    public UserDBManagerTest() throws SQLException {
-        super();
-        dbManager = new UserDBManager();
+    @Before
+    public void openConnection() throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream input = classLoader.getResourceAsStream("sqlCreateScriptTest.sql");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+
+        Statement statement = connection.createStatement();
+        statement.execute(out.toString());
+
+        System.out.println("Successfully loaded test db");
+
+        dbManager = new UserDBManager(connection);
     }
 
     @Test
-    @Ignore
     public void testCreateAndGetUser() throws SQLException {
         final User user = User.builder()
                 .firstName("Firstname")
