163f820052216549512101a2ce6c55976bf1b7c5
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-06 15:52:04 +0100

    Change to resteasy webtarget

--- tdt4140-gr1809/app.client/src/main/java/tdt4140/gr1809/app/client/BasicClient.java
+++ tdt4140-gr1809/app.client/src/main/java/tdt4140/gr1809/app/client/BasicClient.java
@@ -1,16 +1,17 @@
 package tdt4140.gr1809.app.client;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public abstract class BasicClient {
-    protected final WebTarget target;
+    protected final ResteasyWebTarget target;
 
-    public BasicClient(final String resource) {
+    public BasicClient() {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream input = classLoader.getResourceAsStream("client.properties");
         Properties properties = new Properties();
@@ -20,10 +21,13 @@ public abstract class BasicClient {
             e.printStackTrace();
         }
 
-        final String serverUri = properties.getProperty("serverurl")
-                .concat(resource);
+        final String serverUri = properties.getProperty("serverurl");
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        target = client.target(serverUri);
+    }
 
-        final Client client = ClientBuilder.newClient();
+    public BasicClient(final String serverUri) {
+        ResteasyClient client = new ResteasyClientBuilder().build();
         target = client.target(serverUri);
     }
 }
