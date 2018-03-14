49b626d245f9a1699b81ff1089f8a7d2246b7b7e
Christian Bernard Bouwhuis Røkke <cbrokke@stud.ntnu.no>
2018-03-14 14:24:48 +0100

    Added functionality to get related providers

--- tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/dbmanager/ServiceProviderDBManager.java
+++ tdt4140-gr1809/app.server/src/main/java/tdt4140/gr1809/app/server/dbmanager/ServiceProviderDBManager.java
@@ -3,9 +3,12 @@ package tdt4140.gr1809.app.server.dbmanager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import tdt4140.gr1809.app.core.model.DataPoint;
 import tdt4140.gr1809.app.core.model.ServiceProvider;
 
 public class ServiceProviderDBManager extends DBManager {
@@ -47,5 +50,27 @@ public class ServiceProviderDBManager extends DBManager {
     	statement.setTimestamp("birthDate", serviceProvider.getBirthDate());
 		statement.getStatement().executeUpdate();
     }
+	
+	public List<ServiceProvider> getRelatedServiceProviders(UUID userId) throws SQLException{
+		String query = "SELECT firstName, lastName, gender, birthDate from ServiceProviders " +
+				"INNER JOIN ServiceProviderAccessToUser ON ServiceProviders.ServiceProviderId = ServiceProviderAccessToUser.ServiceProviderId " +
+				"WHERE userId = :userId:";
+		NamedParameterStatement statement = new NamedParameterStatement(query, conn);
+		statement.setString("userId", userId.toString());
+		ResultSet result = statement.getStatement().executeQuery();
+		List<ServiceProvider> serviceProviders = new ArrayList<>();
+		while(result.next()) {
+			final ServiceProvider sp =  ServiceProvider.builder()
+					.id(UUID.fromString(result.getString("serviceProviderId")))
+					.firstName(result.getString("firstName"))
+					.lastName(result.getString("lastName"))
+					.birthDate(result.getTimestamp("dataTime").toLocalDateTime())
+					.gender(result.getString("gender"))
+					.build();
+			serviceProviders.add(sp);
+		}
+		return serviceProviders;
+	}
+		
 
 }
