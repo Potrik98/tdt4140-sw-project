43ed570cc3ac53a232eff9a96c7ff40975e476d7
potrik98 <potrik98@gmail.com>
2018-03-21 12:47:15 +0100

    Add abstract generator with implementations

---
+++ tdt4140-gr1809/app.datagen/src/main/java/tdt4140/gr1809/app/datagen/generator/AbstractGenerator.java
@@ -0,0 +1,46 @@
+package tdt4140.gr1809.app.datagen.generator;
+
+import com.google.common.collect.Streams;
+import tdt4140.gr1809.app.core.model.DataPoint;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public abstract class AbstractGenerator {
+    protected final UUID userId;
+    protected final DataPoint.DataType dataType;
+    protected final Random random;
+
+    protected AbstractGenerator(final UUID userId,
+                                final DataPoint.DataType dataType) {
+        this.userId = userId;
+        this.dataType = dataType;
+        random = new Random();
+    }
+
+    public List<DataPoint> generateDataPoints(final int count) {
+        return Streams.zip(
+                Stream.iterate(
+                        LocalDateTime.now(),
+                        time -> time.minus(dataType.timePeriod)
+                ).limit(count),
+                generateRandomValues(count).stream(),
+                (time, value) -> DataPoint.builder()
+                        .time(time)
+                        .value(value)
+                        .userId(userId)
+                        .dataType(dataType)
+                        .build()
+        ).collect(Collectors.toList());
+    }
+
+    protected int getRandomNumberInInterval(int a, int b) {
+        return a + random.nextInt(b - a);
+    }
+
+    public abstract List<Integer> generateRandomValues(final int count);
+}
