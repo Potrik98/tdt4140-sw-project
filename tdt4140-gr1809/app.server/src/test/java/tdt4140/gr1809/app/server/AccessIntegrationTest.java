77cd0f6f90cd71c9ced2b484402058b4d8ff7964
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-03-22 17:07:03 +0100

    Fix Access integration test

--- tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/AccessIntegrationTest.java
+++ tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/AccessIntegrationTest.java
@@ -14,43 +14,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static tdt4140.gr1809.app.server.IntegrationTestHelper.*;
 
 public class AccessIntegrationTest {
-	 @BeforeClass
-	    public static void setupIntegrationTest() throws Exception {
-	        IntegrationTestHelper.setupIntegrationTest();
-	    }
+    @BeforeClass
+    public static void setupIntegrationTest() throws Exception {
+        IntegrationTestHelper.setupIntegrationTest();
+    }
 
-	    @AfterClass
-	    public static void closeConnections() throws Exception {
-	        IntegrationTestHelper.stopIntegrationTest();
-	    }
-	    
-	    @Test
-	    public void testGrantAndGetAccessToUsers() {
-	    	final User user = User.builder()
-	                .firstName("FirstName")
-	                .lastName("LastName")
-	                .birthDate(LocalDateTime.now())
-	                .gender("gender")
-	                .build();
-	        userClient.createUser(user);
-	        
-	        final ServiceProvider serviceProvider = ServiceProvider.builder()
-	                .firstName("FirstName")
-	                .lastName("LastName")
-	                .birthDate(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS))
-	                .gender("gender")
-	                .build();
-	        serviceProviderClient.createServiceProvider(serviceProvider);
-	        
-	        accessClient.giveServiceProviderAccessToUser(serviceProvider.getId(), user.getId());
-	        
-	    	final List<ServiceProvider> userRelation = accessClient.getServiceProviderWithAccessToUser(user.getId());
-	    	
-	    	assertThat(userRelation).usingFieldByFieldElementComparator()
-					.containsExactly(serviceProvider);
-	    }
-	    
-	    
-	    
+    @AfterClass
+    public static void closeConnections() throws Exception {
+        IntegrationTestHelper.stopIntegrationTest();
+    }
 
+    @Test
+    public void testGrantAndGetAccessToUsers() {
+        final User user = User.builder()
+                .firstName("FirstName")
+                .lastName("LastName")
+                .birthDate(LocalDateTime.now())
+                .gender("gender")
+                .build();
+        userClient.createUser(user);
+
+        final ServiceProvider serviceProvider = ServiceProvider.builder()
+                .firstName("FirstName")
+                .lastName("LastName")
+                .birthDate(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS))
+                .gender("gender")
+                .build();
+        serviceProviderClient.createServiceProvider(serviceProvider);
+
+        accessClient.giveServiceProviderAccessToUser(serviceProvider.getId(), user.getId());
+
+        final List<ServiceProvider> serviceProviderWithAccessToUser =
+                accessClient.getServiceProviderWithAccessToUser(user.getId());
+
+        assertThat(serviceProviderWithAccessToUser).usingFieldByFieldElementComparator()
+                .containsExactly(serviceProvider);
+    }
 }
