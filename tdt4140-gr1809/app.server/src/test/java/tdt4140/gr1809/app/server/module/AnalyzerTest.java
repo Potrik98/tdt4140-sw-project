4e0a92e71e38a02b1ca0f138a1161876295afa62
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-04-03 17:43:39 +0200

    Add test for body temperature check

--- tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/module/AnalyzerTest.java
+++ tdt4140-gr1809/app.server/src/test/java/tdt4140/gr1809/app/server/module/AnalyzerTest.java
@@ -8,11 +8,10 @@ import tdt4140.gr1809.app.core.model.DataPoint;
 import tdt4140.gr1809.app.core.model.Notification;
 import tdt4140.gr1809.app.core.model.User;
 import tdt4140.gr1809.app.server.IntegrationTestHelper;
-import tdt4140.gr1809.app.server.resource.NotificationResource;
-import tdt4140.gr1809.app.server.resource.UserResource;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static tdt4140.gr1809.app.server.dbmanager.DBManager.notificationDBManager;
@@ -81,5 +80,61 @@ public class AnalyzerTest {
         assertThat(notification.getMessage()).isEqualTo("Pulse was over max-pulse at "
                 + dataPointHeartRateValueOverMaxPulse.getTime()
                 + ".\nValue: " + dataPointHeartRateValueOverMaxPulse.getValue());
+    }
+
+    @Test
+    public void testGeneratingNotificationsForTemperature() throws Exception {
+        final int lowTemperature = 3750;
+        final int highTemperature = 3800;
+
+        final User user = User.builder()
+                .firstName("Firstname")
+                .lastName("Lastname")
+                .birthDate(LocalDateTime.now())
+                .gender("gender")
+                .build();
+        userDBManager.createUser(user);
+
+        final DataPoint dataPointTemperatureHighValue = DataPoint.builder()
+                .dataType(DataPoint.DataType.TEMPERATURE)
+                .value(highTemperature + 10)
+                .time(LocalDateTime.now())
+                .userId(user.getId())
+                .build();
+
+        final DataPoint dataPointTemperatureLowValue = DataPoint.builder()
+                .dataType(DataPoint.DataType.TEMPERATURE)
+                .value(lowTemperature - 10)
+                .time(LocalDateTime.now())
+                .userId(user.getId())
+                .build();
+
+        final DataPoint dataPointTemperatureMediumValue = DataPoint.builder()
+                .dataType(DataPoint.DataType.TEMPERATURE)
+                .value((lowTemperature + highTemperature) / 2)
+                .time(LocalDateTime.now())
+                .userId(user.getId())
+                .build();
+
+        analyzer.analyzeDataPoints(ImmutableList.of(
+                dataPointTemperatureHighValue,
+                dataPointTemperatureLowValue,
+                dataPointTemperatureMediumValue));
+
+        final List<Notification> notificationsForUser =
+                notificationDBManager.getNotificationByUserId(user.getId());
+
+        assertThat(notificationsForUser).hasSize(2);
+
+        final List<String> notificationMessages = notificationsForUser.stream()
+                .map(Notification::getMessage)
+                .collect(Collectors.toList());
+
+        assertThat(notificationMessages).containsExactlyInAnyOrder(
+                "Low body temperature at " + dataPointTemperatureLowValue.getTime()
+                        + ".\nValue: " + dataPointTemperatureLowValue.getValue(),
+                "High body temperature at " + dataPointTemperatureHighValue.getTime()
+                        + ".\nValue: " + dataPointTemperatureHighValue.getValue()
+        );
     }
 }
