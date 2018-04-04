d7543478f8fa94657274676d26a3fba4a6d69e31
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-04-04 13:54:29 +0200

    Change from duration to period

--- tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/util/TimeUtils.java
+++ tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/util/TimeUtils.java
@@ -1,12 +1,12 @@
 package tdt4140.gr1809.app.core.util;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 
 public class TimeUtils {
-    public static boolean localDateTimesAreWithinADurationOfEachOther(final LocalDateTime a,
+    public static boolean localDateTimesAreWithinAPeriodOfEachOther(final LocalDateTime a,
                                                                       final LocalDateTime b,
-                                                                      final Duration duration) {
-        return a.isBefore(b.plus(duration)) && a.isAfter(b.minus(duration));
+                                                                      final Period period) {
+        return a.isBefore(b.plus(period)) && a.isAfter(b.minus(period));
     }
 }
