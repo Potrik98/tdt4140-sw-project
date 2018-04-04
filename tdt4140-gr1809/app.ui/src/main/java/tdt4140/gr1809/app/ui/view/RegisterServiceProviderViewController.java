8bb5cfe3a263a5422fc0080760042986aa4db02b
filiphag <filiphag@stud.ntnu.no>
2018-04-04 12:29:02 +0200

    skrivefiel i kode

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/view/RegisterServiceProviderViewController.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/view/RegisterServiceProviderViewController.java
@@ -60,7 +60,7 @@ public class RegisterServiceProviderViewController implements Initializable {
 	@Override
 	public void initialize(final URL url, final ResourceBundle rb) {
 		genderChoiceBox.getItems().addAll("Male", "Female");
-		genderChoiceBox.setValue("Maleee");
+		genderChoiceBox.setValue("Male");
 
 	}
 
