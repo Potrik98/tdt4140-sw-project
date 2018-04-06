a986c08bbfe5477c385fe02e0cf6b0b32c388d6a
filiphag <filiphag@stud.ntnu.no>
2018-04-06 14:45:30 +0200

    sender opp eksempel note nå

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/view/NotificationsViewController.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/view/NotificationsViewController.java
@@ -8,19 +8,22 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.util.StringConverter;
 import tdt4140.gr1809.app.client.NotificationClient;
+import tdt4140.gr1809.app.client.UserClient;
 import tdt4140.gr1809.app.ui.FxAppController;
 import tdt4140.gr1809.app.core.model.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 public class NotificationsViewController implements Initializable{
 
 	private FxAppController fxAppController;
 	@FXML ListView<Notification> NotificationsList;
-	@FXML TextArea Notification;
+	@FXML TextArea NotificationText;
 	private NotificationClient client = new NotificationClient();
 	
 	@Override
@@ -31,11 +34,21 @@ public class NotificationsViewController implements Initializable{
 	
 	public void updateNotification() {
 		Notification note = NotificationsList.getSelectionModel().getSelectedItem();
-		Notification.setText(note.getMessage());
+		NotificationText.setText(note.getMessage());
 	}
 	
 	public void setfxAppController(FxAppController controller) {
 		fxAppController = controller;
+		
+		final Notification eksempel = Notification.builder()
+			.message("DU MÅ TIL LEGEN DU HAR PULS OVER 9000 BPM!!!")
+			.userId(fxAppController.user.getId())
+			.id(UUID.randomUUID())
+			.time(LocalDateTime.now())
+			.build();
+		NotificationClient client2 = new NotificationClient();
+		client2.createNotification(eksempel);
+		
 		List<Notification> notificationList = client.getNotificationByUserId(fxAppController.user.getId());
 		NotificationsList.setItems(FXCollections.observableArrayList(notificationList));
 	}
