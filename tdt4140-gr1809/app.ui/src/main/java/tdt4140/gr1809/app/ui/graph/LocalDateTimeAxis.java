225daca797a4c0dfd364baa3923c5d370e851bdf
potrik98 <potrik98@gmail.com>
2018-04-13 00:41:36 +0200

    Add NumerableAxis

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/graph/LocalDateTimeAxis.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/graph/LocalDateTimeAxis.java
@@ -1,50 +1,29 @@
 package tdt4140.gr1809.app.ui.graph;
 
-import com.google.common.collect.ImmutableList;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.BooleanPropertyBase;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.DoublePropertyBase;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.StringPropertyBase;
 import javafx.beans.value.ChangeListener;
-import javafx.geometry.Dimension2D;
-import javafx.geometry.Side;
-import javafx.scene.chart.ValueAxis;
 import javafx.util.StringConverter;
-import tdt4140.gr1809.app.core.util.NumberUtils;
 import tdt4140.gr1809.app.core.value.LocalDateTimeNumberConverter;
-import tdt4140.gr1809.app.core.value.Numerable;
+import tdt4140.gr1809.app.core.value.NumberConverter;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
 
-public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
-    private static final Numerable.NumerableBuilder<LocalDateTime> localDateTimeNumerableBuilder =
-            new Numerable.NumerableBuilder<>(new LocalDateTimeNumberConverter());
+public class LocalDateTimeAxis extends NumerableAxis<LocalDateTime> {
+    private static final NumberConverter<LocalDateTime> localDateTimeNumberConverter =
+            new LocalDateTimeNumberConverter();
 
-    private static class Range {
-        final long lowerBound;
-        final long upperBound;
-        final double tickUnit;
-        final double scale;
+    protected class LocalDateTimeRange extends NumerableAxis.Range {
         final String format;
-        Range(final long lowerBound,
-              final long upperBound,
-              final double tickUnit,
-              final double scale,
-              final String format) {
-            this.lowerBound = lowerBound;
-            this.upperBound = upperBound;
-            this.tickUnit = tickUnit;
-            this.scale = scale;
+        protected LocalDateTimeRange(final double lowerBound,
+                                     final double upperBound,
+                                     final double tickUnit,
+                                     final double scale,
+                                     final String format) {
+            super(lowerBound, upperBound, tickUnit, scale);
             this.format = format;
         }
     }
@@ -80,30 +59,6 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
     }
 
     /*
-     * When true zero is always included in the visible range. This only has effect if auto-ranging is on.
-     */
-    private BooleanProperty forceZeroInRange = new BooleanPropertyBase(true) {
-        @Override protected void invalidated() {
-            // This will effect layout if we are auto ranging
-            if(isAutoRanging()) requestAxisLayout();
-        }
-
-        @Override
-        public Object getBean() {
-            return LocalDateTimeAxis.this;
-        }
-
-        @Override
-        public String getName() {
-            return "forceZeroInRange";
-        }
-    };
-
-    public final boolean isForceZeroInRange() { return forceZeroInRange.getValue(); }
-    public final void setForceZeroInRange(boolean value) { forceZeroInRange.setValue(value); }
-    public final BooleanProperty forceZeroInRangeProperty() { return forceZeroInRange; }
-
-    /*
      * The format used for the tick unit marks
      */
     private StringProperty tickUnitFormat = new StringPropertyBase("dd-MMM HH:mm") {
@@ -118,38 +73,13 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
         }
     };
 
-    /*
-     * The value between each major tick mark in data units. This is automatically set if we are auto-ranging.
-     */
-    private DoubleProperty tickUnit = new DoublePropertyBase(5) {
-        @Override protected void invalidated() {
-            if(!isAutoRanging()) {
-                invalidateRange();
-                requestAxisLayout();
-            }
-        }
-
-        @Override
-        public Object getBean() {
-            return LocalDateTimeAxis.this;
-        }
-
-        @Override
-        public String getName() {
-            return "tickUnit";
-        }
-    };
-
-    private double getTickUnit() { return tickUnit.get(); }
-    private void setTickUnit(final double value) { tickUnit.set(value); }
-    public final DoubleProperty tickUnitProperty() { return tickUnit; }
-
     /**
      * Creates an auto-ranging time axis
      */
     public LocalDateTimeAxis() {
+        super(localDateTimeNumberConverter);
         forceZeroInRange.set(false);
-        setTickLabelFormatter(getDefaultFormatter());
+        setTickLabelFormatter(getDefaultTickLabelFormatter());
     }
 
     /**
@@ -160,49 +90,22 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
      * @param tickUnit The tick unit
      */
     public LocalDateTimeAxis(final double lowerBound, final double upperBound, final double tickUnit) {
-        super(lowerBound, upperBound);
-        setTickUnit(tickUnit);
+        super(localDateTimeNumberConverter, lowerBound, upperBound, tickUnit);
     }
 
     /**
      * Create a non-auto-ranging LocalDateTimeAxis with the given upper bound, lower bound and tick unit
      *
-     * @param axisLabel The name to display for this axis
      * @param lowerBound The lower bound for this axis
      * @param upperBound The upper bound for this axis
      * @param tickUnit The tick unit
+     * @param axisLabel The name to display for this axis
      */
-    public LocalDateTimeAxis(final String axisLabel,
-                             final double lowerBound,
+    public LocalDateTimeAxis(final double lowerBound,
                              final double upperBound,
-                             final double tickUnit) {
-        super(lowerBound, upperBound);
-        setTickUnit(tickUnit);
-        setLabel(axisLabel);
-    }
-
-    /**
-     * Calculate a list of the data values for every minor tick mark
-     *
-     * @return List of data values where to draw minor tick marks
-     */
-    @Override
-    protected List<Numerable<LocalDateTime>> calculateMinorTickMarks() {
-        final double minorUnit = getTickUnit() / getMinorTickCount();
-        if (minorUnit > 0 && getUpperBound() > getLowerBound()) {
-            return DoubleStream.iterate(getLowerBound(), d -> d + minorUnit)
-                    .limit((long) getUpperBound())
-                    .mapToObj(localDateTimeNumerableBuilder::numerableOfDouble)
-                    .collect(Collectors.toList());
-        } else if (getUpperBound() < getLowerBound()) {
-            return DoubleStream.iterate(getUpperBound(), d -> d - minorUnit)
-                    .limit((long) getLowerBound())
-                    .map(d -> -d)
-                    .mapToObj(localDateTimeNumerableBuilder::numerableOfDouble)
-                    .collect(Collectors.toList());
-        } else {
-            return ImmutableList.of();
-        }
+                             final double tickUnit,
+                             final String axisLabel) {
+        super(localDateTimeNumberConverter, lowerBound, upperBound, tickUnit, axisLabel);
     }
 
     /**
@@ -215,17 +118,11 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
      */
     @Override
     protected void setRange(final Object rangeObject, final boolean animate) {
-        if (!(rangeObject instanceof Range)) {
-            throw new IllegalArgumentException("Range object must be of type Range!");
+        super.setRange(rangeObject, animate);
+        if (!(rangeObject instanceof LocalDateTimeRange)) {
+            throw new IllegalArgumentException("Range object must be of type LocalDateTimeRange!");
         }
-        final Range range = (Range) rangeObject;
-
-        final double oldLowerBound = getLowerBound();
-        setLowerBound(range.lowerBound);
-        setUpperBound(range.upperBound);
-        setTickUnit(range.tickUnit);
-        currentLowerBound.set(range.lowerBound);
-        setScale(range.scale);
+        final LocalDateTimeRange range = (LocalDateTimeRange) rangeObject;
         tickUnitFormat.setValue(range.format);
     }
 
@@ -235,7 +132,7 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
      */
     @Override
     protected Object getRange() {
-        return new Range(
+        return new LocalDateTimeRange(
                 (long) getLowerBound(),
                 (long) getUpperBound(),
                 getTickUnit(),
@@ -244,45 +141,7 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
     }
 
     /**
-     * Calculate a list of all the data values for each tick mark in range
-     *
-     * @param length The length of the axis in display units
-     * @param rangeObject A range object returned from autoRange()
-     * @return A list of tick marks that fit along the axis if it was the given length
-     */
-    @Override
-    protected List<Numerable<LocalDateTime>> calculateTickValues(double length, Object rangeObject) {
-        if (!(rangeObject instanceof Range)) {
-            throw new IllegalArgumentException("Range object must be of type Range!");
-        }
-        final Range range = (Range) rangeObject;
-
-        if (range.tickUnit > 0 && range.upperBound > range.lowerBound) {
-            return DoubleStream.iterate(range.lowerBound, d -> d + range.tickUnit)
-                    .limit(range.upperBound)
-                    .mapToObj(localDateTimeNumerableBuilder::numerableOfDouble)
-                    .collect(Collectors.toList());
-        } else if (range.upperBound < range.lowerBound) {
-            return DoubleStream.iterate(range.upperBound, d -> d - range.tickUnit)
-                    .limit(range.lowerBound)
-                    .map(d -> -d)
-                    .mapToObj(localDateTimeNumerableBuilder::numerableOfDouble)
-                    .collect(Collectors.toList());
-        } else {
-            return ImmutableList.of();
-        }
-    }
-
-    @Override
-    protected String getTickMarkLabel(final Numerable<LocalDateTime> value) {
-        final StringConverter<Numerable<LocalDateTime>> formatter = Objects.isNull(getTickLabelFormatter())
-                ? getDefaultFormatter()
-                : getTickLabelFormatter();
-        return formatter.toString(value);
-    }
-
-    /**
-     * Called to set the upper and lower bound and anything else that needs to be auto-ranged
+     * Called to calculate the best range for the min and max data values
      *
      * @param minDataValue The min data value that needs to be plotted on this axis
      * @param maxDataValue The max data value that needs to be plotted on this axis
@@ -294,96 +153,48 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
                                          final double maxDataValue,
                                          final double length,
                                          final double labelSize) {
-        final Side side = getSide();
-        final boolean vertical = Side.LEFT.equals(side) || Side.RIGHT.equals(side);
-
-        // If zero is not in the range, and we're forcing zero, set zero to one of the end points.
-        final double maxValue = isForceZeroInRange() && maxDataValue < 0 ? 0 : maxDataValue;
-        final double minValue = isForceZeroInRange() && minDataValue > 0 ? 0 : minDataValue;
-
-        final double range = maxValue - minValue;
-
-        // calculate the number of tick-marks we can fit in the given length
-        final int numberOfTickMarks = (int) NumberUtils.clamp(2, 20,
-                Math.floor(Math.abs(length) / labelSize));
-
-        // calculate tick unit for the number of ticks can have in the given data range
-        final double tickUnit = range / (double) numberOfTickMarks;
+        final Range range = (Range) super.autoRange(minDataValue, maxDataValue, length, labelSize);
 
         // search for the best tick unit that fits
         final TickUnitDefault tickUnitDefault = Arrays.stream(TickUnitDefault.values())
-                .min(Comparator.comparingDouble(def -> Math.abs(def.tickUnit - tickUnit)))
+                .min(Comparator.comparingDouble(def -> Math.abs(def.tickUnit - range.tickUnit)))
                 .orElse(TickUnitDefault.HOURS_1);
 
         final double newTickUnit = tickUnitDefault.tickUnit;
-        final long lowerBounds = Math.round(Math.floor(minValue / newTickUnit) * newTickUnit);
-        final long upperBounds = Math.round(Math.ceil(maxValue / newTickUnit) * newTickUnit);
+        final long lowerBound = Math.round(Math.floor(range.lowerBound / newTickUnit) * newTickUnit);
+        final long upperBound = Math.round(Math.ceil(range.upperBound / newTickUnit) * newTickUnit);
 
         // calculate new scale
-        final double newScale = calculateNewScale(length, lowerBounds, upperBounds);
+        final double newScale = calculateNewScale(length, lowerBound, upperBound);
 
         // return new range
-        return new Range(lowerBounds, upperBounds, tickUnit, newScale, tickUnitDefault.format);
+        return new LocalDateTimeRange(lowerBound, upperBound, range.tickUnit, newScale, tickUnitDefault.format);
     }
 
-    /**
-     * Measure the size of the label for given tick mark value. This uses the font that is set for the tick marks
-     *
-     * @param value tick mark value
-     * @param rangeObject range to use during calculations
-     * @return size of tick mark label for given value
-     */
-    @Override protected Dimension2D measureTickMarkSize(final Numerable<LocalDateTime> value, final Object rangeObject) {
-        if (!(rangeObject instanceof Range)) {
-            throw new IllegalArgumentException("Range object must be of type Range!");
-        }
-        final Range range = (Range) rangeObject;
-        final StringConverter<Numerable<LocalDateTime>> formatter = Objects.isNull(getTickLabelFormatter())
-                ? getDefaultFormatter(range)
-                : getTickLabelFormatter();
-        final String labelText = formatter.toString(value);
-        return measureTickMarkLabelSize(labelText, getTickLabelRotation());
-    }
-
-    private DefaultFormatter getDefaultFormatter(final Range range) {
-        return new DefaultFormatter(this, range);
-    }
-
-    private DefaultFormatter getDefaultFormatter() {
-        return new DefaultFormatter(this);
+    @Override
+    protected DefaultLocalDateTimeStringConverter getDefaultStringConverter() {
+        return new DefaultLocalDateTimeStringConverter(this);
     }
 
     /**
      * Default number formatter for LocalDateTimeAxis.
      * Stays in sync with auto-ranging and formats values appropriately.
      */
-    public static class DefaultFormatter extends StringConverter<Numerable<LocalDateTime>> {
+    private static class DefaultLocalDateTimeStringConverter extends StringConverter<LocalDateTime> {
         private DateTimeFormatter formatter;
 
         /**
-         * Construct a DefaultFormatter for the given LocalDateTimeAxis
+         * Construct a DefaultLocalDateTimeStringConverter for the given LocalDateTimeAxis
          *
          * @param axis The axis to format tick marks for
          */
-        DefaultFormatter(final LocalDateTimeAxis axis) {
-            formatter = getFormatter(axis);
+        DefaultLocalDateTimeStringConverter(final LocalDateTimeAxis axis) {
+            formatter = DateTimeFormatter.ofPattern(axis.tickUnitFormat.getValue());
 
             final ChangeListener<String> axisListener =
-                    (observable, oldValue, newValue) -> formatter = getFormatter(axis);
+                    (observable, oldValue, newValue) -> formatter = DateTimeFormatter.ofPattern(newValue);
 
             axis.tickUnitFormat.addListener(axisListener);
-        }
-
-        /**
-         * Construct a DefaultFormatter for a given range
-         */
-        DefaultFormatter(final LocalDateTimeAxis axis, final Range range) {
-            this(axis);
-            formatter = DateTimeFormatter.ofPattern(range.format);
-        }
-
-        private static DateTimeFormatter getFormatter(final LocalDateTimeAxis axis) {
-            return DateTimeFormatter.ofPattern(axis.tickUnitFormat.getValue());
         }
 
         /**
@@ -393,8 +204,8 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
          * @see StringConverter#toString
          */
         @Override
-        public String toString(final Numerable<LocalDateTime> localDateTimeNumerable) {
-            return formatter.format(localDateTimeNumerable.getValue());
+        public String toString(final LocalDateTime localDateTime) {
+            return formatter.format(localDateTime);
         }
 
         /**
@@ -404,8 +215,8 @@ public class LocalDateTimeAxis extends ValueAxis<Numerable<LocalDateTime>> {
          * @see StringConverter#toString
          */
         @Override
-        public Numerable<LocalDateTime> fromString(final String string) {
-            return localDateTimeNumerableBuilder.numerableOfValue(LocalDateTime.parse(string, formatter));
+        public LocalDateTime fromString(final String string) {
+            return LocalDateTime.parse(string, formatter);
         }
     }
 }
