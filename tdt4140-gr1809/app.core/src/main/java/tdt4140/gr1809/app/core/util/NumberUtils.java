6529b44ccaea1e1b88e75da8c8115f3d13dc4450
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-04-12 15:57:55 +0200

    Add numberUtils

---
+++ tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/util/NumberUtils.java
@@ -0,0 +1,17 @@
+package tdt4140.gr1809.app.core.util;
+
+public class NumberUtils {
+    /**
+     * Clamps a value between two bounds
+     * 
+     * @param lowerBound lower bound for the value
+     * @param upperBound upper bound for the value
+     * @param value the value to clamp to the interval
+     * @return clamped value
+     */
+    public static double clamp(final double lowerBound,
+                               final double upperBound,
+                               final double value) {
+        return Math.max(lowerBound, Math.min(upperBound, value));
+    }
+}
