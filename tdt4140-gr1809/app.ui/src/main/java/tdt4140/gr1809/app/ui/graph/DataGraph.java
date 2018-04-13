3ec6fa1467e6966166e2eb372f006fdb38a6014b
potrik98 <potrik98@gmail.com>
2018-04-13 03:08:51 +0200

    Use numerable in dataGraph

--- tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/graph/DataGraph.java
+++ tdt4140-gr1809/app.ui/src/main/java/tdt4140/gr1809/app/ui/graph/DataGraph.java
@@ -1,10 +1,13 @@
 package tdt4140.gr1809.app.ui.graph;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import tdt4140.gr1809.app.core.model.DataPoint;
+import tdt4140.gr1809.app.core.value.LocalDateTimeNumberConverter;
+import tdt4140.gr1809.app.core.value.Numerable;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -14,31 +17,31 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class DataGraph {
-    private final XYChart<LocalDateTime, Number> graph;
-    private final Map<DataPoint.DataType, XYChart.Series<LocalDateTime, Number>> seriesMap;
+    private static final Numerable.NumerableBuilder<LocalDateTime> numerableBuilder =
+            new Numerable.NumerableBuilder<>(new LocalDateTimeNumberConverter());
+
+    private final XYChart<Numerable<LocalDateTime>, Number> graph;
+    private final Map<DataPoint.DataType, XYChart.Series<Numerable<LocalDateTime>, Number>> seriesMap;
 
     public DataGraph() {
-        this.graph = new LineChart<>(new LocalDateTimeAxis().getUnwrappedAxis(), new NumberAxis());
+        this.graph = new LineChart<>(new LocalDateTimeAxis(), new NumberAxis());
         seriesMap = Arrays.stream(DataPoint.DataType.values())
                 .collect(Collectors.toMap(
                         Function.identity(),
-                        a -> new XYChart.Series<LocalDateTime, Number>()));
-        graph.setData(seriesMap.values().stream()
-                .collect(Collectors.toCollection(FXCollections::observableArrayList)));
-        System.out.println("Data was set");
+                        a -> new XYChart.Series<Numerable<LocalDateTime>, Number>()));
     }
 
-    public XYChart<LocalDateTime, Number> getGraph() {
+    public XYChart<Numerable<LocalDateTime>, Number> getGraph() {
         return graph;
+    }
+
+    public void plotDataType(final DataPoint.DataType dataType) {
+        graph.setData(FXCollections.observableArrayList(seriesMap.get(dataType)));
     }
 
     public void addDataPoints(final List<DataPoint> dataPoints) {
         dataPoints.forEach(dataPoint -> seriesMap.get(dataPoint.getDataType()).getData()
-                .add(new XYChart.Data<>(dataPoint.getTime(), dataPoint.getValue())));
-        graph.setData(seriesMap.values().stream()
-                .collect(Collectors.toCollection(FXCollections::observableArrayList)));
-        System.out.println("Data was set");
-        System.out.println("Added " + dataPoints.size() + " datapoints");
+                .add(new XYChart.Data<Numerable<LocalDateTime>, Number>(numerableBuilder.numerableOfValue(dataPoint.getTime()), dataPoint.getValue())));
     }
 
     public void clear() {
