a9e20e556201a5e9e51e2df78cc9af25a2044b08
Erling Skaarseth Rørvik <erlingsr@stud.ntnu.no>
2018-04-13 14:11:29 +0200

    Add average to timeUtils

--- tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/util/TimeUtils.java
+++ tdt4140-gr1809/app.core/src/main/java/tdt4140/gr1809/app/core/util/TimeUtils.java
@@ -1,12 +1,26 @@
 package tdt4140.gr1809.app.core.util;
 
+import tdt4140.gr1809.app.core.value.LocalDateTimeNumberConverter;
+
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.ZoneOffset;
+import java.util.List;
 
 public class TimeUtils {
+    private final static LocalDateTimeNumberConverter localDateTimeNumberConverter
+            = new LocalDateTimeNumberConverter();
+
     public static boolean localDateTimesAreWithinAPeriodOfEachOther(final LocalDateTime a,
                                                                     final LocalDateTime b,
                                                                     final Period period) {
         return a.isBefore(b.plus(period)) && a.isAfter(b.minus(period));
+    }
+
+    public static LocalDateTime averageLocalDateTime(final List<LocalDateTime> localDateTimes) {
+        return localDateTimeNumberConverter.ofLongValue(
+                localDateTimes.stream()
+                    .mapToLong(localDateTimeNumberConverter::toLongValue)
+                    .sum() / localDateTimes.size());
     }
 }
